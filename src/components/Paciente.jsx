63d05b096b853370902f7efe6fda3a060a469884
Victor Vikhal <alfonso.vikhal@gmail.com>
2023-04-06 17:11:52 +0200

    Estilos de nuestra primera app terminados/90%

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,31 @@
+
+
+const Paciente = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+    <p className="font-bold mb-3 text-gray-700 uppercase">
+      Nombre: {""}
+      <span className="font-normal normal-case">Igor</span>
+    </p>
+    <p className="font-bold mb-3 text-gray-700 uppercase">
+      Propietario: {""}
+      <span className="font-normal normal-case">Victor</span>
+    </p>
+    <p className="font-bold mb-3 text-gray-700 uppercase">
+      Email: {""}
+      <span className="font-normal normal-case">correo@corre.com</span>
+    </p>
+    <p className="font-bold mb-3 text-gray-700 uppercase">
+      Fecha Alta: {""}
+      <span className="font-normal normal-case">10 Diciembre de 2023</span>
+    </p>
+    <p className="font-bold mb-3 text-gray-700 uppercase">
+      Sintomas: {""}
+      <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores voluptates repellat quas hic quasi. Inventore, officiis? Possimus sed enim pariatur impedit velit assumenda mollitia cum odit harum, nisi perferendis. Vero.</span>
+    </p>
+    
+  </div>
+  )
+}
+
+export default Paciente
