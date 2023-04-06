eede34203e5a0f0cfeecf8f13402229d2691690d
Victor Vikhal <alfonso.vikhal@gmail.com>
2023-04-06 11:58:42 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [ "./index.html", "./src/**/*.jsx" ], //Entra al directorio src y escanea todos los archivos que tenga ".jsx"
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
